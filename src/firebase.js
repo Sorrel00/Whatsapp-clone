866ee975c93eadc84b65112251b642e955f9f002
Sorrel <sorrelrolly@gmail.com>
2024-04-09 17:31:08 +0100

    add of icons DonutLarge Chat and MoreVert in the Sidebar.js file

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyDLMK3PwSgll3Nf40G7CX60LCRVeHweD68",
+  
+    authDomain: "whats-app-clone-2ee47.firebaseapp.com",
+  
+    projectId: "whats-app-clone-2ee47",
+  
+    storageBucket: "whats-app-clone-2ee47.appspot.com",
+  
+    messagingSenderId: "966672725761",
+  
+    appId: "1:966672725761:web:3fb13faaf7eaa251dbcd00",
+  
+    measurementId: "G-QEF1QD72WY"
+  
+  };
