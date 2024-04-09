f95d0d7b07aa180b98c8402361387d36e0482157
Sorrel <sorrelrolly@gmail.com>
2024-04-09 19:07:14 +0100

    add of icon profile

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,9 +1,11 @@
 import React from 'react'
-import {Avatar} from '@mui/material'
+import {Avatar, IconButton} from '@mui/material'
 import DonutLargeIcon from '@mui/icons-material/DonutLarge';
 import ChatIcon from '@mui/icons-material/Chat';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
+import SearchOutlined from '@mui/icons-material/SearchOutlined';
 import './Sidebar.css'
+import SidebarChat from './SidebarChat.js'
 
 
 function Sidebar(){
@@ -12,15 +14,33 @@ function Sidebar(){
             <div className="sidebar__header">
                 <Avatar/>
                 <div className="sidebar__headerRight">
-                    <DonutLargeIcon />
-                    <ChatIcon />
-                    <MoreVertIcon />
+                    <IconButton>
+                        <DonutLargeIcon />
+                    </IconButton>
+
+                    <IconButton>
+                        <ChatIcon />
+                    </IconButton>
+
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton> 
                 </div>
             </div>
 
-            <div className="sidebar__search"></div>
+            <div className="sidebar__search">
+                <div className="sidebar__searchContainer">
+                    <SearchOutlined/>
+                    <input placeholder="Search or start new chat" type="text"/>
+                </div>
+            </div>
 
-            <div className="sidebar__chats"></div>
+            <div className="sidebar__chats">
+                <SidebarChat/>
+                <SidebarChat/>
+                <SidebarChat/>
+                <SidebarChat/>
+            </div>
         </div>
     )
 }
